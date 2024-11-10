181c04a167fb275b0677782bf69083d415842b5d
Alina <alinalalalaa@gmail.com>
2024-11-10 14:53:53 +0300

    Adjust TodoItem key prop

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
- const TodoItem: React.FC<{taskText: string, key: string}> = (props) => {
+ const TodoItem: React.FC<{taskText: string, index: string}> = (props) => {
     return(
-        <li className="todos__task" key={props.key}>
+        <li className="todos__task" key={props.index}>
             <label className="todos__task-label">
                 <span className="todos__task-check"></span>
                 <input className="todos__task-input" type="checkbox" />
